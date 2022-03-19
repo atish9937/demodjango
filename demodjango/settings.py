70410542716e9e8f8d6816707409cd8ec663f19b
Atish Patel <atish9937@gmail.com>
2022-03-19 00:26:15 -0700

    Added web.config and allowed host as * in settings.py

--- demodjango/settings.py
+++ demodjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+g=3l70v^508@gpg22o#d_@5kf5lj8&)_r&x#-u4mm&(p856ht'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
